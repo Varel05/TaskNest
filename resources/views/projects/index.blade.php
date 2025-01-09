a4dda724b2eebc0bbe9fb1b648fb409c2e7e78dd
Pavelekaa <edwinpaveleka@gmail.com>
2025-01-09 21:26:10 +0700

    styling index project

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,18 +1,16 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row">
+<div class="container mx-auto px-4 py-8">
+    <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         @foreach ($projects as $project)
-            <div class="col-md-6 mb-4">
-                <div class="card" onclick="window.location='{{ route('projects.show', $project->id) }}'" style="cursor: pointer;">
-                    <div class="card-body">
-                        <h5 class="card-title">{{ $project->name }}</h5>
-                        <p class="card-text">
-                            <strong>End Date:</strong> {{ $project->end_date }}<br>
-                            <strong>Created By:</strong> {{ $project->user->name }}
-                        </p>
-                    </div>
+            <div class="bg-white dark:bg-gray-800 shadow-lg rounded-lg overflow-hidden cursor-pointer transform hover:scale-105 transition duration-300" onclick="window.location='{{ route('projects.show', $project->id) }}'">
+                <div class="p-6">
+                    <h5 class="text-xl font-semibold text-gray-800 dark:text-gray-100 mb-2">{{ $project->name }}</h5>
+                    <p class="text-gray-600 dark:text-gray-300 text-sm">
+                        <strong class="font-medium text-gray-800 dark:text-gray-100">End Date:</strong> {{ $project->end_date }}<br>
+                        <strong class="font-medium text-gray-800 dark:text-gray-100">Created By:</strong> {{ $project->user->name }}
+                    </p>
                 </div>
             </div>
         @endforeach
